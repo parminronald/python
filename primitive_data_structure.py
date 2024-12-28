3f4644f02e8017eb95d96f1b58ea1a2ae0fcdb85
parminronald <parmin.ronald@gmail.com>
2024-12-28 15:33:20 +0700

    add primitive_data_structure.py

---
+++ primitive_data_structure.py
@@ -0,0 +1,27 @@
+# primitive data structure in python {string, integer, float, boolean}
+# "string" is a sequence of characters exapmle: "python", "1234", "1.234" etc
+# "integer" is a whole number exapmle: 1, 100, 2, 20, 3, 4 etc
+# "float" is a decimal number exapmle: 1.23, 2.34, 3.45, 4.56 etc
+# "boolean" is a True or False exapmle: True, False
+
+# example of string code
+name = "python"
+version = "3.7.4"
+print(f"My Name is {name} and version is {version}")
+
+# example of integer code
+age = 25
+print(f"My name is {name}, My Age is {age}")
+
+# example of float code
+price_coffee = 1.23
+buy_coffee = 2
+total_price = price_coffee * buy_coffee
+print(f"Price of Coffee is {price_coffee}, Buy Coffee {buy_coffee}, Total Price is {total_price}")
+
+# example of boolean code
+is_student = True
+if is_student:
+    print("Student")
+else:    
+    print("Not Student")    
