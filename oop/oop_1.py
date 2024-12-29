8e766c3f52d74713880cad3aa5a897ae9d796cc8
parminronald <parmin.ronald@gmail.com>
2024-12-30 05:39:21 +0700

    add opp folder

---
+++ oop/oop_1.py
@@ -0,0 +1,10 @@
+class Mobil:
+    def __init__(self, merek, model, warna):
+        self.merek = merek
+        self.model = model
+        self.warna = warna
+# Memanggil instance dari Class Mobil
+instance_mobil = Mobil("toyota", "manual", "putih")
+print(f"Merek mobil: {instance_mobil.merek}")
+print(f"Model : {instance_mobil.model}")
+print(f"Warna: {instance_mobil.warna}")
