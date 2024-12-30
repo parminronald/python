45e0eb5bfbffc011deaae23a23e5cdf29f0d50ea
parminronald <parmin.ronald@gmail.com>
2024-12-30 15:42:24 +0700

    list

---
+++ list.py
@@ -0,0 +1,14 @@
+"""
+Keyword
+1. prepend(item) menambahkan item dari depan;
+2. append(item) menambahkan item dari belakang.
+3. insert(index, item) menambahkan item dari indeks tertentu
+"""
+fruits_list = ["Anggur", "Pepaya", "Jeruk"]
+mixed_list = ["Python", 13.4, 10, True]
+# to Access first fruits_list (Anggur), Which is index [0]
+first_fruit = fruits_list[1]
+print(f"First index 0 the fruits_list is: {first_fruit}") # Output Pepaya
+second_fruit = fruits_list[0]
+
+print (fruits_list)
